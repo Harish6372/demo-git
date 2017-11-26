a770a5897c22b03d7416b41356f20f8db1034336
sureharish <sureharish63@gmail.com>
2017-11-26 09:20:34 +0530

    addding java files

---
+++ src/main/java/com/nisum/macyd/nisumJersyDemo/NisumJersyDemoApplication.java
@@ -0,0 +1,12 @@
+package com.nisum.macyd.nisumJersyDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NisumJersyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NisumJersyDemoApplication.class, args);
+	}
+}
