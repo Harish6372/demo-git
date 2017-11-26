05f144a27af5c94962a9240ebd23bd9a14ffbcdd
sureharish <sureharish63@gmail.com>
2017-11-26 09:31:36 +0530

    file modifed

--- src/main/java/com/nisum/macyd/nisumJersyDemo/NisumJersyDemoApplication.java
+++ src/main/java/com/nisum/macyd/nisumJersyDemo/NisumJersyDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NisumJersyDemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("sating lskbs");
 		SpringApplication.run(NisumJersyDemoApplication.class, args);
 	}
 }
